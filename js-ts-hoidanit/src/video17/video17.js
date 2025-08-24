0b51a1c4633f3faa294bc144d4cad74325e56a69
= <=>
2025-08-24 19:50:33 +0700

    First commit

---
+++ js-ts-hoidanit/src/video17/video17.js
@@ -0,0 +1,5 @@
+console.log("Video 17");
+var name = "HarutoLearnJS";
+console.log(name);
+
+
---
+++ js-ts-hoidanit/src/video18/video18.js
@@ -0,0 +1,5 @@
+console.log("Video 18");
+// number
+const age = 25;
+const MyAge = 30;
+console.log(age, MyAge);
