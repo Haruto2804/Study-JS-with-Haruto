8569329c66c16fd4c9921f1ca404f4252dabd9a2
= <=>
2025-08-25 08:59:25 +0700

    OK

--- js-ts-hoidanit/src/video18/video18.js
+++ js-ts-hoidanit/src/video18/video18.js
@@ -3,3 +3,17 @@ console.log("Video 18");
 const age = 25;
 const MyAge = 30;
 console.log(age, MyAge);
+//string
+const name = "Haruto";
+const name1 = "Sinestrea";
+const name2 = "HaruLuvSines";
+console.log(name, name1, name2);
+//boolean: true/false
+const isTuiDepTrai = true;
+console.log(isTuiDepTrai);
+// undefine
+let haruto;
+console.log(haruto); // --> xuat hien undefine
+// null
+let testNull = null;
+console.log(testNull);
---
+++ js-ts-hoidanit/src/video19/video19.js
@@ -0,0 +1,15 @@
+console.log("Video 19");
+
+// 2 dạng tham chiếu dùng nhiều nhất
+
+// 1. Object tương tự struct bên C++
+const person = {
+    name: "haruto",
+    age: 20,
+    address: "TPHCM"
+}
+console.log(person);
+
+//array, array bên JS dùng dấu []
+const scores = [10, 3, 4, 9]
+console.log(scores);
