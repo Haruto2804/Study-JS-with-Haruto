230c2d8099a28f2a306dcc14f8b8d79e5abcd8eb
= <=>
2025-08-25 18:59:05 +0700

    Video 27 28

--- js-ts-hoidanit/src/Chapter 4/video27/video27.js
+++ js-ts-hoidanit/src/Chapter 4/video27/video27.js
@@ -1,1 +1,17 @@
 console.log("Video 27");
+
+// SWITCH CASE
+const scores = 10;
+switch (true) {
+    case (scores >= 8 && scores <= 10):
+        console.log("Giỏi!!!");
+        break;
+    case (scores >= 6 && scores < 8):
+        console.log("Khá");
+        break;
+    case (scores >= 4 && scores < 6):
+        console.log("Trung Bình!!");
+        break;
+    default:
+        console.log("Kém!!");
+}
---
+++ js-ts-hoidanit/src/Chapter 4/video28/video28.js
@@ -0,0 +1,21 @@
+console.log("Video 28!!");
+// vòng lặp for
+for (let i = 0; i <= 6; i++) {
+    console.log(`i= ${i}`);
+}
+console.log("=================");
+
+// vòng lặp while
+let scores = 10;
+while (scores > 5) {
+    console.log("check score >> ", scores);
+    scores--;
+}
+
+// do while
+console.log("=================");
+let score1 = 10;
+do {
+    console.log(" check score1 >> ", score1);
+    score1--;
+} while (score1 > 5);
